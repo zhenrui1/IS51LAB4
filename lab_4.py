0ad83e5e4d96a66b0bf2d4759e454ff3685d3875
zhenrui1 <zhenrui1219@mail.fresnostate.edu>
2022-05-07 15:05:14 -0700

    finish lab 4

--- lab_4.py
+++ lab_4.py
@@ -38,6 +38,23 @@ if not correct
 main
 """
 
+def main():
+    question = "What is the capital of California? "
+    answer = "Sacramento"
+    ask(question, answer)
 
+def ask(question, answer, max_tries=3):
+    tries = 0
+    ans = ""
+    while tries < max_tries:
+        tries = tries + 1 
+        ans = input(question)
+        if ans == answer:
+            print("Correct!")
+            break
+    if ans != answer:
+        print("You have used up your allotment of guesses.")
+
+main()
 
 
